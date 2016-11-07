241a9b3b39ebbd5c22a49dfd2c685855a7c0e0ca
Matthew Dee <matt@Matthews-MacBook-Air.local>
2016-11-08 11:01:23 +1300

    Now with actual expoting.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# flib
-Foggerty's Library
+flib
+Foggerty's Library.
+
+Basic library with stuff that I'll be using again.  So far just a couple of helper functions to be used at the application (not package) level.
--- flib.go
+++ flib.go
@@ -1,11 +1,13 @@
 package flib
 
 import (
-	"os"
 	"fmt"
+	"os"
 )
 
-func abortIf(test func() bool, alsoDo func()) {
+// AbortIf will abort execution if test returns false.  If alsoDo is
+// not nil, will execute that first.
+func AbortIf(test func() bool, alsoDo func()) {
 	if !test() {
 		if alsoDo != nil {
 			alsoDo()
@@ -15,7 +17,10 @@ func abortIf(test func() bool, alsoDo func()) {
 	}
 }
 
-func abortIfErr(test func() error, msg string, alsoDo func()) {
+// AbortIfErr will abort execution if test returns an error, after
+// dumping the error to stderr.  If alsoDo is not nil, will execute
+// that before aborting.
+func AbortIfErr(test func() error, msg string, alsoDo func()) {
 	err := test()
 
 	if err != nil {
@@ -30,5 +35,5 @@ func abortIfErr(test func() error, msg string, alsoDo func()) {
 }
 
 func dumpErr(msg string, err error) {
-	fmt.Printf("Something went horribly wrong:\n%s\n%s\n", msg, err.Error())
+	fmt.Fprintf(os.Stderr, "Something went horribly wrong:\n%s\n%s\n", msg, err.Error())
 }
